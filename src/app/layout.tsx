0c59c4f100e9c80fa9c0638919fb6776dd7bc105
Saher Rameez <165132831+sahergul@users.noreply.github.com>
2024-10-09 19:09:06 +0500

    Update layout.tsx
    update unused import image delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
